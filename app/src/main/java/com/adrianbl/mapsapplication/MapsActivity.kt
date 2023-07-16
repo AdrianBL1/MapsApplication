5d0e80eb2da1754aed32b1867b58adde4370ceb3
Adrian Basilio López <BL_980713@hotmail.com>
2023-07-15 23:43:24 -0600

    Update variable names and minor details

--- app/src/main/java/com/adrianbl/mapsapplication/MapsActivity.kt
+++ app/src/main/java/com/adrianbl/mapsapplication/MapsActivity.kt
@@ -42,7 +42,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
-    private val baresCubles = mutableListOf<BaresClubes>()
+    private val baresClubes = mutableListOf<BaresClubes>()
     private lateinit var beerIcon: BitmapDescriptor
     private val userLocation = Location("")
 
@@ -64,273 +64,273 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR BARECITO",
                 19.94549471,
                 -96.85168242
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR CENTRO BOTANERO EL CENTARIO",
                 19.92974498,
                 -96.8537597
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR CLIMA",
                 19.92858363,
                 -96.84999181
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR DOS HERMANOS",
                 19.92431576,
                 -96.85695598
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL GATO NEGRO",
                 19.92347404,
                 -96.85864603
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL PATIO",
                 19.92304699,
                 -96.85426504
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL SESTEO DE LAS AGUILAS",
                 19.93739192,
                 -96.85003448
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EMPERADOR",
                 19.92651621,
                 -96.85454088
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR GARCIS",
                 19.92542172,
                 -96.85750936
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR JUMBO",
                 19.92538877,
                 -96.85697468
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA BARRA DE DANTE",
                 19.93012421,
                 -96.85276356
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CASA DE LA SEÑORA DE LAS PLANTAS",
                 19.920365837133392,
                 -96.85823830555884
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CATRINA",
                 19.93000678,
                 -96.84961932
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CUEVA",
                 19.9307954,
                 -96.8493769
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CHENTA",
                 19.921614085889736,
                 -96.86226161909204
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA JAROCHA",
                 19.92880431,
                 -96.85355029
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA NUEVA MISANTECA",
                 19.93020844,
                 -96.8495968
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA SIRENITA",
                 19.92681269,
                 -96.85241676
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR MARY",
                 19.9295132,
                 -96.84967294
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BARES, CANTINAS Y SIMILARES",
                 19.93144367,
                 -96.8487903
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BODEGA AUXILIAR DE VINOS Y LICORES",
                 19.92538877,
                 -96.85697468
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BOTANERO LOS COCOS",
                 19.91950348,
                 -96.86044087
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BOTANERO D'FILI",
                 19.9462378033694,
                 -96.85061873815745
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CENTRO BOTANERO SIN NOMBRE",
                 19.92478609,
                 -96.85494674
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA EL REFUGIO DE LA QUETA",
                 19.9267405,
                 -96.85390018
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA JACIVE",
                 19.92706836,
                 -96.84079529
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERÍA LOS CERRITOS",
                 19.94438675,
                 -96.84890846
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MEMOS",
                 19.92652592,
                 -96.85445044
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MEMOS",
                 19.92652592,
                 -96.85445044
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MORELOS",
                 19.9232807,
                 -96.85415998
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA ROMADHI",
                 19.9442837858024,
                 -96.85012328832784
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA SIN NOMBRE",
                 19.93746933,
                 -96.85134989
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA SIN NOMBRE",
                 19.92347453,
                 -96.85888094
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CHELOS BAR",
                 19.92506305,
                 -96.85755458
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "COFFE BAR LA SOPA",
                 19.92581066,
                 -96.85434734
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "DISCOTHEQUE XANAT",
                 19.92791185,
                 -96.85494739
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "EL BAR DEL PUEBLO",
                 19.931305015204657,
                 -96.84891611337663
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "ICE CLUB",
                 19.92938289,
                 -96.84958032
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "MERENDERO LOS REYES",
                 19.93030139,
@@ -419,19 +419,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val userMarker = MarkerOptions().position(userLatLng)
         mMap.addMarker(userMarker)
 
-        for (baresCubles in baresCubles) {
-            val barclubPosition = LatLng(baresCubles.latitud, baresCubles.longitude)
+        for (baresClubes in baresClubes) {
+            val barclubPosition = LatLng(baresClubes.latitud, baresClubes.longitude)
             val barclubLocation = Location("")
 
-            barclubLocation.latitude = baresCubles.latitud
-            barclubLocation.longitude = baresCubles.longitude
+            barclubLocation.latitude = baresClubes.latitud
+            barclubLocation.longitude = baresClubes.longitude
 
             val distanceToIcon = barclubLocation.distanceTo(userLocation)
 
             val barclubMarkerOptions = MarkerOptions()
                 .icon(beerIcon)
                 .position(barclubPosition)
-                .title(baresCubles.name)
+                .title(baresClubes.name)
                 .snippet(getString(R.string.distance_to_format, distanceToIcon))
             mMap.addMarker(barclubMarkerOptions)
         }
@@ -440,7 +440,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.setOnMarkerClickListener { marker ->
             // Se itera sobre los objetos BaresClubes en la lista
-            for (baresClubes in baresCubles) {
+            for (baresClubes in baresClubes) {
                 // Crear un objeto LatLng para las coordenadas del objeto BaresClubes
                 val baresClubesLatLng = LatLng(baresClubes.latitud, baresClubes.longitude)
 
--- app/src/main/java/com/adrianbl/mapsapplication/PanelPrincipal.kt
+++ app/src/main/java/com/adrianbl/mapsapplication/PanelPrincipal.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.adrianbl.mapsapplication.databinding.ActivityMapsBinding
 import com.adrianbl.mapsapplication.databinding.ActivityPanelPrincipalBinding
 
 class PanelPrincipal : AppCompatActivity() {
@@ -12,7 +11,7 @@ class PanelPrincipal : AppCompatActivity() {
     private lateinit var binding: ActivityPanelPrincipalBinding
 
     private lateinit var adapter: MyAdapter
-    private val baresCubles = mutableListOf<BaresClubes>()
+    private val baresClubes = mutableListOf<BaresClubes>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,273 +22,273 @@ class PanelPrincipal : AppCompatActivity() {
             startActivity(Intent(this, MapsActivity::class.java))
         }
 
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR BARECITO",
                 19.94549471,
                 -96.85168242
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR CENTRO BOTANERO EL CENTARIO",
                 19.92974498,
                 -96.8537597
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR CLIMA",
                 19.92858363,
                 -96.84999181
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR DOS HERMANOS",
                 19.92431576,
                 -96.85695598
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL GATO NEGRO",
                 19.92347404,
                 -96.85864603
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL PATIO",
                 19.92304699,
                 -96.85426504
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EL SESTEO DE LAS AGUILAS",
                 19.93739192,
                 -96.85003448
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR EMPERADOR",
                 19.92651621,
                 -96.85454088
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR GARCIS",
                 19.92542172,
                 -96.85750936
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR JUMBO",
                 19.92538877,
                 -96.85697468
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA BARRA DE DANTE",
                 19.93012421,
                 -96.85276356
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CASA DE LA SEÑORA DE LAS PLANTAS",
                 19.920365837133392,
                 -96.85823830555884
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CATRINA",
                 19.93000678,
                 -96.84961932
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CUEVA",
                 19.9307954,
                 -96.8493769
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA CHENTA",
                 19.921614085889736,
                 -96.86226161909204
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA JAROCHA",
                 19.92880431,
                 -96.85355029
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA NUEVA MISANTECA",
                 19.93020844,
                 -96.8495968
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR LA SIRENITA",
                 19.92681269,
                 -96.85241676
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BAR MARY",
                 19.9295132,
                 -96.84967294
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BARES, CANTINAS Y SIMILARES",
                 19.93144367,
                 -96.8487903
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BODEGA AUXILIAR DE VINOS Y LICORES",
                 19.92538877,
                 -96.85697468
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BOTANERO LOS COCOS",
                 19.91950348,
                 -96.86044087
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "BOTANERO D'FILI",
                 19.9462378033694,
                 -96.85061873815745
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CENTRO BOTANERO SIN NOMBRE",
                 19.92478609,
                 -96.85494674
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA EL REFUGIO DE LA QUETA",
                 19.9267405,
                 -96.85390018
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA JACIVE",
                 19.92706836,
                 -96.84079529
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERÍA LOS CERRITOS",
                 19.94438675,
                 -96.84890846
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MEMOS",
                 19.92652592,
                 -96.85445044
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MEMOS",
                 19.92652592,
                 -96.85445044
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA MORELOS",
                 19.9232807,
                 -96.85415998
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA ROMADHI",
                 19.9442837858024,
                 -96.85012328832784
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA SIN NOMBRE",
                 19.93746933,
                 -96.85134989
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CERVECERIA SIN NOMBRE",
                 19.92347453,
                 -96.85888094
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "CHELOS BAR",
                 19.92506305,
                 -96.85755458
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "COFFE BAR LA SOPA",
                 19.92581066,
                 -96.85434734
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "DISCOTHEQUE XANAT",
                 19.92791185,
                 -96.85494739
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "EL BAR DEL PUEBLO",
                 19.931305015204657,
                 -96.84891611337663
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "ICE CLUB",
                 19.92938289,
                 -96.84958032
             )
         )
-        baresCubles.add(
+        baresClubes.add(
             BaresClubes(
                 "MERENDERO LOS REYES",
                 19.93030139,
@@ -297,7 +296,7 @@ class PanelPrincipal : AppCompatActivity() {
             )
         )
 
-        adapter = MyAdapter(baresCubles)
+        adapter = MyAdapter(baresClubes)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
     }
